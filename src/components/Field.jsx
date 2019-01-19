71ff81d48eb3d57a9b8f3de6ceabcf2088f022b4
Joshua Webb <bbew.auhsoj+git@gmail.com>
2019-01-19 11:43:06 +1100

    [perf] Only re-render field when it changes
    Drastically reduce the number of objects that get created just for
    React to compare them, find out they are the same and throw them
    away.

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,58 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Field.css';
 
 import { fieldWidthInBlocks, fieldHeightInBlocks,
          blockSizeInUnits } from '../constants'
 
-const Field = (props) => {
-  const borderStyle = {
-    fill: 'transparent',
-    strokeWidth:1,
-    stroke: '#FFFFFF',
-  };
-  // TODO: take from outside / make less arbitrary
-  const width = blockSizeInUnits * fieldWidthInBlocks;
-  const height = blockSizeInUnits * fieldHeightInBlocks;
+class Field extends Component {
 
-  const yOffset = 100;
-  const xLeft = width / -2;
-  const yTop = -yOffset-height;
-
-  var selectedRow = props.dude.row;
-  var selectedCol = props.dude.col;
-
-  var grid = [];
-  for (var r = 0; r < fieldHeightInBlocks; r++) {
-    for (var c = 0; c < fieldWidthInBlocks; c++) {
-      const id = `${r}-${c}`;
-      var gridBlock = <rect
-          id={id}
-          key={id}
-          x={xLeft + c * blockSizeInUnits}
-          y={yTop + r * blockSizeInUnits}
-          width={blockSizeInUnits}
-          height={blockSizeInUnits}
-          onClick={props.click.bind(this, r, c)}
-          className={"grid-block " + ((r === selectedRow && c === selectedCol) ? "grid-block-flash" : "") }
-        />;
-      grid.push(gridBlock);
-    }
+  shouldComponentUpdate(nextProps, nextState) {
+    const { dude } = this.props;
+    return dude.row !== nextProps.dude.row ||
+           dude.col !== nextProps.dude.col;
   }
 
-  return (
-    <g id="field">
-      {grid}
-      <rect
-        id="border"
-        style={borderStyle}
-        x={xLeft}
-        y={yTop}
-        width={width}
-        height={height}
-        pointerEvents="none"
-      />
-    </g>
-  );
+  render() {
+    const borderStyle = {
+      fill: 'transparent',
+      strokeWidth:1,
+      stroke: '#FFFFFF',
+    };
+    // TODO: take from outside / make less arbitrary
+    const width = blockSizeInUnits * fieldWidthInBlocks;
+    const height = blockSizeInUnits * fieldHeightInBlocks;
+
+    const yOffset = 100;
+    const xLeft = width / -2;
+    const yTop = -yOffset-height;
+
+    var selectedRow = this.props.dude.row;
+    var selectedCol = this.props.dude.col;
+
+    var grid = [];
+    for (var r = 0; r < fieldHeightInBlocks; r++) {
+      for (var c = 0; c < fieldWidthInBlocks; c++) {
+        const id = r * fieldWidthInBlocks + c;
+        var gridBlock = <rect
+            id={id}
+            key={id}
+            x={xLeft + c * blockSizeInUnits}
+            y={yTop + r * blockSizeInUnits}
+            width={blockSizeInUnits}
+            height={blockSizeInUnits}
+            onClick={this.props.click.bind(this, r, c)}
+            className={"grid-block " + ((r === selectedRow && c === selectedCol) ? "grid-block-flash" : "") }
+          />;
+        grid.push(gridBlock);
+      }
+    }
+
+    return (
+      <g id="field">
+        {grid}
+        <rect
+          id="border"
+          style={borderStyle}
+          x={xLeft}
+          y={yTop}
+          width={width}
+          height={height}
+          pointerEvents="none"
+        />
+      </g>
+    );
+  }
 };
 
 export default Field;
--- src/reducers/highlightDude.js
+++ src/reducers/highlightDude.js
@@ -1,11 +1,9 @@
 function highlightDude(state, action) {
-  if (action.dude.row === -1) {
-    return state;
-  }
-
   return {
     ...state,
-    dude: action.dude,
+    dude: {
+        ...action.dude
+    },
   };
 }
 
