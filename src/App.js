c3cc2d4157cb6522e206db32763d2339f1d6cb81
Joshua Webb <bbew.auhsoj+git@gmail.com>
2019-02-09 16:39:50 +1100

    Cleanup #2
    Remove initial keyboard processing code, in the future we may end up
    implementing proper keyboard controls but for now we're focusing on
    touch controls and don't need to waste time/resources on partially
    implemented code that does nothing.

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ class App extends Component {
     this.initialiseLayout();
 
     this.keydown = this.keydown.bind(this);
-    this.keyup = this.keyup.bind(this);
     this.gameLoop = this.gameLoop.bind(this);
     this.selectOrientation = this.selectOrientation.bind(this);
     this.updateOrientation = this.updateOrientation.bind(this);
@@ -122,13 +121,6 @@ class App extends Component {
     });
   }
 
-  processKey(button, isDown) {
-    if (button.isDown !== isDown) {
-      button.halfTransitionCount++;
-    }
-    button.isDown = isDown;
-  }
-
   // TODO: We should change the name of one of these
   // to disambiguate it from the action that actually
   // updates the state/container.
@@ -139,14 +131,6 @@ class App extends Component {
   keydown(event) {
     // TODO: configurable keys
     switch (event.keyCode) {
-      // 'a'
-      case 65: this.processKey(this.newGameInput.left, true); break;
-      // 'd'
-      case 68: this.processKey(this.newGameInput.right, true); break;
-      // 's'
-      case 83: this.processKey(this.newGameInput.down, true); break;
-      // 'w'
-      case 87: this.processKey(this.newGameInput.up, true); break;
       // ',' (<)
       case 188:
         this.pieceDebug.next = false;
@@ -161,37 +145,8 @@ class App extends Component {
     }
   }
 
-  keyup(event) {
-    // TODO: configurable keys
-    switch (event.keyCode) {
-      // 'a'
-      case 65: this.processKey(this.newGameInput.left, false); break;
-      // 'd'
-      case 68: this.processKey(this.newGameInput.right, false); break;
-      // 's'
-      case 83: this.processKey(this.newGameInput.down, false); break;
-      // 'w'
-      case 87: this.processKey(this.newGameInput.up, false); break;
-      // no default
-    }
-  }
-
   init() {
     // internal state
-    this.oldGameInput = {
-      left : { isDown: false, halfTransitionCount: 0 },
-      right: { isDown: false, halfTransitionCount: 0 },
-      up   : { isDown: false, halfTransitionCount: 0 },
-      down : { isDown: false, halfTransitionCount: 0 },
-    };
-
-    this.newGameInput = {
-      left : { isDown: false, halfTransitionCount: 0 },
-      right: { isDown: false, halfTransitionCount: 0 },
-      up   : { isDown: false, halfTransitionCount: 0 },
-      down : { isDown: false, halfTransitionCount: 0 },
-    };
-
     this.pieceDebug = {
       next : false,
       prev : false,
@@ -567,19 +522,6 @@ class App extends Component {
 
     this.props.updateTimer(this.totalTimerMillis);
 
-    // save the input from this frame
-    // maintain the `isDown` state across frames
-    var temp = this.oldGameInput;
-    this.oldGameInput = this.newGameInput;
-    this.newGameInput = temp;
-    this.newGameInput.left.isDown = this.oldGameInput.left.isDown;
-    this.newGameInput.left.halfTransitionCount = 0;
-    this.newGameInput.right.isDown = this.oldGameInput.right.isDown;
-    this.newGameInput.right.halfTransitionCount = 0;
-    this.newGameInput.up.isDown = this.oldGameInput.up.isDown;
-    this.newGameInput.up.halfTransitionCount = 0;
-    this.newGameInput.down.isDown = this.oldGameInput.down.isDown;
-    this.newGameInput.down.halfTransitionCount = 0;
   }
 
   componentDidMount() {
@@ -589,14 +531,12 @@ class App extends Component {
     window.onresize();
 
     document.addEventListener("keydown", this.keydown, false);
-    document.addEventListener("keyup", this.keyup, false);
 
     setInterval(this.gameLoop, 33);
   }
 
   componentWillUnmount() {
     document.removeEventListener("keydown", this.keydown);
-    document.removeEventListener("keyup", this.keyup);
   }
 
   render() {
